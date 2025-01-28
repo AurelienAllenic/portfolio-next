7cff055e7860948eb4c91743bd79c2ffe3afa336
AurelienAllenic <aurelien.allenic@gmail.com>
2025-01-28 16:26:12 +0100

    (feat) added style for projects page

--- prisma/addProjects.ts
+++ prisma/addProjects.ts
@@ -6,7 +6,7 @@ async function main() {
   const projects = [
     {
       image: 'https://aurelienallenic.fr/assets/booki-CA3PqGpU.webp',
-      title: 'Booki, projet de site de réservation hôtelier réalisé en HTML/CSS',
+      title: 'Booki',
       description: 'Un projet de site web de réservation d’hôtels réalisé avec HTML et CSS.',
       category: 'Web',
       objectifs: 'Créer un site de réservation simple pour les hôtels.',
@@ -15,7 +15,7 @@ async function main() {
     },
     {
       image: 'https://aurelienallenic.fr/assets/ohmyfood-Dqbwe2yt.jpg',
-      title: 'Ohmyfood, site gastronomique réalisé en HTML, CSS et SCSS',
+      title: 'Ohmyfood',
       description: 'Un site web gastronomique utilisant HTML, CSS et SCSS pour une meilleure présentation.',
       category: 'Web',
       objectifs: 'Mettre en place un site de recettes gastronomiques.',
@@ -24,7 +24,7 @@ async function main() {
     },
     {
       image: 'https://aurelienallenic.fr/assets/lapanthere-AMUkuVkG.jpg',
-      title: "Lapanthere, site de web design basée à Lyon, projet d'amélioration SEO d'un site existant",
+      title: "Lapanthere",
       description: 'Amélioration du SEO d’un site web de design basé à Lyon.',
       category: 'SEO',
       objectifs: 'Améliorer la visibilité d’un site web dans les résultats des moteurs de recherche.',
@@ -33,7 +33,7 @@ async function main() {
     },
     {
       image: 'https://aurelienallenic.fr/assets/kanap-CEFHtTxJ.webp',
-      title: 'Kanap, site e-commerce réalisé en JavaScript, HTML, CSS',
+      title: 'Kanap',
       description: 'Développement d’un site e-commerce avec JavaScript, HTML et CSS.',
       category: 'E-commerce',
       objectifs: 'Créer un site de vente en ligne de canapés.',
@@ -42,7 +42,7 @@ async function main() {
     },
     {
       image: 'http://aurelienallenic.fr/assets/piiquante-CxSmnMkl.webp',
-      title: 'Hot-takes, API de like et partage de sauces pimentées réalisée en NodeJs et en JavaScript',
+      title: 'Hot-takes',
       description: 'Création d’une API permettant de liker et partager des sauces pimentées.',
       category: 'API',
       objectifs: 'Construire une API RESTful en utilisant NodeJs.',
@@ -51,7 +51,7 @@ async function main() {
     },
     {
       image: 'http://aurelienallenic.fr/assets/piiquante-CxSmnMkl.webp',
-      title: "Groupomania, réseau social d'entreprise réalisé avec ReactJs et NodeJs",
+      title: "Groupomania",
       description: 'Réseau social d’entreprise permettant aux employés de communiquer.',
       category: 'Réseau social',
       objectifs: 'Créer une plateforme de communication interne pour les entreprises.',
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+'use client'; // Indiquer que ce composant est côté client
 
 import { FC, useEffect, useState } from 'react';
 import { fetchProjects } from '../api/action';
@@ -7,101 +7,122 @@ import './styles.scss';
 import { Project } from '../types';
 
 const Projects: FC = () => {
-    const [projects, setProjects] = useState<Project[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
-    const [filteredProjects, setFilteredProjects] = useState<Project[]>([]);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const [filteredProjects, setFilteredProjects] = useState<Project[]>([]);
 
-    const [selectedTechnology, setSelectedTechnology] = useState<string>('');
-    const [selectedCategory, setSelectedCategory] = useState<string>('');
+  const [selectedTechnology, setSelectedTechnology] = useState<string>('');
+  const [selectedCategory, setSelectedCategory] = useState<string>('');
 
-    useEffect(() => {
+  const [activeProject, setActiveProject] = useState<number | null>(null);  // État pour suivre le projet survolé
+
+  useEffect(() => {
     const loadProjects = async () => {
-        try {
+      try {
         const data = await fetchProjects();
         setProjects(data);
         setFilteredProjects(data);
-        } catch (err) {
+      } catch (err) {
         console.log(err);
         setError('Failed to fetch projects');
-        } finally {
+      } finally {
         setLoading(false);
-        }
+      }
     };
 
     loadProjects();
-    }, []);
+  }, []);
 
-    useEffect(() => {
+  useEffect(() => {
     let filtered = projects;
 
     if (selectedTechnology) {
-        filtered = filtered.filter(project =>
+      filtered = filtered.filter(project =>
         project.technologies.includes(selectedTechnology)
-        );
+      );
     }
 
     if (selectedCategory) {
-        filtered = filtered.filter(project =>
+      filtered = filtered.filter(project =>
         project.category.toLowerCase() === selectedCategory.toLowerCase()
-        );
+      );
     }
 
     setFilteredProjects(filtered);
-    }, [selectedTechnology, selectedCategory, projects]);
+  }, [selectedTechnology, selectedCategory, projects]);
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>{error}</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
 
-    const allCategories = Array.from(new Set(projects.map(project => project.category)));
-    const allTechnologies = Array.from(
+  const allCategories = Array.from(new Set(projects.map(project => project.category)));
+  const allTechnologies = Array.from(
     new Set(projects.flatMap(project => project.technologies))
-    );
+  );
 
-    return (
+  const handleMouseEnter = (id: number) => {
+    setActiveProject(id);  // Ajoute la classe active en fonction de l'id du projet
+  };
+
+  const handleMouseLeave = () => {
+    setActiveProject(null);  // Enlève la classe active lorsqu'on sort de la carte
+  };
+
+  return (
     <div className='container-page-all-projects'>
-        <h1>Projets</h1>
+      <h1>Nos réalisations</h1>
 
-        <div className='filters'>
+      <div className='filters'>
         <select
-            onChange={(e) => setSelectedTechnology(e.target.value)}
-            value={selectedTechnology}
+          onChange={(e) => setSelectedTechnology(e.target.value)}
+          value={selectedTechnology}
         >
-            <option value=''>Select Technology</option>
-            {allTechnologies.map((tech, index) => (
+          <option value=''>Select Technology</option>
+          {allTechnologies.map((tech, index) => (
             <option key={index} value={tech}>
-                {tech}
+              {tech}
             </option>
-            ))}
+          ))}
         </select>
 
         <select
-            onChange={(e) => setSelectedCategory(e.target.value)}
-            value={selectedCategory}
+          onChange={(e) => setSelectedCategory(e.target.value)}
+          value={selectedCategory}
         >
-            <option value=''>Select Category</option>
-            {allCategories.map((category, index) => (
+          <option value=''>Select Category</option>
+          {allCategories.map((category, index) => (
             <option key={index} value={category}>
-                {category}
+              {category}
             </option>
-            ))}
+          ))}
         </select>
-        </div>
+      </div>
 
       <ul className='container-all-projects'>
         {filteredProjects.map((project) => (
+          <div key={project.id} className='project-container'>
+            {/* Div avec image floutée */}
+
+            {/* Div avec contenu visible */}
             <Link
-                href={`/project/${project.id}`}
-                key={project.id}
-                style={{ background: `url(${project.image}) no-repeat center center / cover` }}
+              href={`/project/${project.id}`}
+              className='project-link'
+              style={{ backgroundImage: `url(${project.image})` }} // L'image visible ici
+              onMouseEnter={() => handleMouseEnter(project.id)} // Ajouter la classe active
+              onMouseLeave={handleMouseLeave} // Retirer la classe active
             >
+              <div
+                className={`containers-infos-project ${activeProject === project.id ? 'active' : 'active'}`}
+              >
                 <p className='project-title'>{project.title}</p>
                 <p className='project-description'>{project.description}</p>
+              </div>
             </Link>
+          </div>  
         ))}
-        </ul>
+      </ul>
     </div>
-    );
+  );
 };
 
 export default Projects;
