28bd6ee498737f3546379045f38f15aa2f3497c5
AurelienAllenic <aurelien.allenic@gmail.com>
2025-01-28 16:36:16 +0100

    (fix) changed indents

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,10 @@
-'use client'; // Indiquer que ce composant est côté client
+"use client";
 
-import { FC, useEffect, useState } from 'react';
-import { fetchProjects } from '../api/action';
-import Link from 'next/link';
-import './styles.scss';
-import { Project } from '../types';
+import { FC, useEffect, useState } from "react";
+import { fetchProjects } from "../api/action";
+import Link from "next/link";
+import "./styles.scss";
+import { Project } from "../types";
 
 const Projects: FC = () => {
   const [projects, setProjects] = useState<Project[]>([]);
@@ -12,10 +12,10 @@ const Projects: FC = () => {
   const [error, setError] = useState<string | null>(null);
   const [filteredProjects, setFilteredProjects] = useState<Project[]>([]);
 
-  const [selectedTechnology, setSelectedTechnology] = useState<string>('');
-  const [selectedCategory, setSelectedCategory] = useState<string>('');
+  const [selectedTechnology, setSelectedTechnology] = useState<string>("");
+  const [selectedCategory, setSelectedCategory] = useState<string>("");
 
-  const [activeProject, setActiveProject] = useState<number | null>(null);  // État pour suivre le projet survolé
+  const [activeProject, setActiveProject] = useState<number | null>(null);
 
   useEffect(() => {
     const loadProjects = async () => {
@@ -25,7 +25,7 @@ const Projects: FC = () => {
         setFilteredProjects(data);
       } catch (err) {
         console.log(err);
-        setError('Failed to fetch projects');
+        setError("Failed to fetch projects");
       } finally {
         setLoading(false);
       }
@@ -38,14 +38,15 @@ const Projects: FC = () => {
     let filtered = projects;
 
     if (selectedTechnology) {
-      filtered = filtered.filter(project =>
+      filtered = filtered.filter((project) =>
         project.technologies.includes(selectedTechnology)
       );
     }
 
     if (selectedCategory) {
-      filtered = filtered.filter(project =>
-        project.category.toLowerCase() === selectedCategory.toLowerCase()
+      filtered = filtered.filter(
+        (project) =>
+          project.category.toLowerCase() === selectedCategory.toLowerCase()
       );
     }
 
@@ -55,29 +56,31 @@ const Projects: FC = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
 
-  const allCategories = Array.from(new Set(projects.map(project => project.category)));
+  const allCategories = Array.from(
+    new Set(projects.map((project) => project.category))
+  );
   const allTechnologies = Array.from(
-    new Set(projects.flatMap(project => project.technologies))
+    new Set(projects.flatMap((project) => project.technologies))
   );
 
   const handleMouseEnter = (id: number) => {
-    setActiveProject(id);  // Ajoute la classe active en fonction de l'id du projet
+    setActiveProject(id);
   };
 
   const handleMouseLeave = () => {
-    setActiveProject(null);  // Enlève la classe active lorsqu'on sort de la carte
+    setActiveProject(null);
   };
 
   return (
-    <div className='container-page-all-projects'>
+    <div className="container-page-all-projects">
       <h1>Nos réalisations</h1>
 
-      <div className='filters'>
+      <div className="filters">
         <select
           onChange={(e) => setSelectedTechnology(e.target.value)}
           value={selectedTechnology}
         >
-          <option value=''>Select Technology</option>
+          <option value="">Select Technology</option>
           {allTechnologies.map((tech, index) => (
             <option key={index} value={tech}>
               {tech}
@@ -89,7 +92,7 @@ const Projects: FC = () => {
           onChange={(e) => setSelectedCategory(e.target.value)}
           value={selectedCategory}
         >
-          <option value=''>Select Category</option>
+          <option value="">Select Category</option>
           {allCategories.map((category, index) => (
             <option key={index} value={category}>
               {category}
@@ -98,27 +101,26 @@ const Projects: FC = () => {
         </select>
       </div>
 
-      <ul className='container-all-projects'>
+      <ul className="container-all-projects">
         {filteredProjects.map((project) => (
-          <div key={project.id} className='project-container'>
-            {/* Div avec image floutée */}
-
-            {/* Div avec contenu visible */}
+          <div key={project.id} className="project-container">
             <Link
               href={`/project/${project.id}`}
-              className='project-link'
-              style={{ backgroundImage: `url(${project.image})` }} // L'image visible ici
-              onMouseEnter={() => handleMouseEnter(project.id)} // Ajouter la classe active
-              onMouseLeave={handleMouseLeave} // Retirer la classe active
+              className="project-link"
+              style={{ backgroundImage: `url(${project.image})` }}
+              onMouseEnter={() => handleMouseEnter(project.id)}
+              onMouseLeave={handleMouseLeave}
             >
               <div
-                className={`containers-infos-project ${activeProject === project.id ? 'active' : 'active'}`}
+                className={`containers-infos-project ${
+                  activeProject === project.id ? "active" : "active"
+                }`}
               >
-                <p className='project-title'>{project.title}</p>
-                <p className='project-description'>{project.description}</p>
+                <p className="project-title">{project.title}</p>
+                <p className="project-description">{project.description}</p>
               </div>
             </Link>
-          </div>  
+          </div>
         ))}
       </ul>
     </div>
