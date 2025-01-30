8ddd7eaf3edba6da22016ff48cf8555180011bcc
enzocosson <enzolemercier@gmail.com>
2025-01-30 10:42:46 +0100

    feat : add new icon for logo

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import styles from "./header.module.scss";
 import Link from "next/link";
+import Image from "next/image";
 
 function Header() {
   return (
     <header className={styles.header}>
       <Link href="/">
-      <div className={styles.logo}>RenderFlow.</div>
-    </Link>
-      <Link href="#contact-form" className={styles.contactButton}>Contactez-nous</Link>
+        <div className={styles.logo}>
+          <Image
+            src="/images/logo-icon.png"
+            alt="logo"
+            width={30}
+            height={30}
+          ></Image>
+          enderFlow.
+        </div>
+      </Link>
+      <Link href="#contact-form" className={styles.contactButton}>
+        Contactez-nous
+      </Link>
     </header>
   );
 }
